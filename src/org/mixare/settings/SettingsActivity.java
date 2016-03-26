4464810d247374a620567839169a2b2e256b5abc
Hanno - Felix Wagner <hanno-felix.wagner@stud.uni-due.de>
2016-03-26 11:50:38 +0100

    finalize work on settings screen add back action add reset function add value display

--- src/org/mixare/settings/SettingsActivity.java
+++ src/org/mixare/settings/SettingsActivity.java
@@ -2,15 +2,29 @@ package org.mixare.settings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         // show the fragment
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
