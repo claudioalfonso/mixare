1bcb8e8ac30d771561896393084f0117a9e21cb4
Melanie <melanie_werner@gmx.net>
2016-04-04 17:30:34 +0200

    changed location point on map + walked color now alterable in settings

--- src/org/mixare/map/MyLocationOverlay.java
+++ src/org/mixare/map/MyLocationOverlay.java
@@ -1,6 +1,7 @@
 package org.mixare.map;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import org.mapsforge.map.model.MapViewPosition;
 import org.mapsforge.core.graphics.Paint;
 import org.mapsforge.core.graphics.Style;
 import org.mixare.MixContext;
+import org.mixare.R;
 
 /**
  * Created by MelanieW on 03.04.2016.
@@ -26,24 +28,21 @@ public class MyLocationOverlay extends Layer implements LocationListener {
 
     private final MapViewPosition mapViewPosition;
     private final Circle circle;
+    private final Circle innerCircle;
     private boolean myLocationEnabled = true;
+    private static final int alpha=Color.argb(128, 0, 0, 0);
+
 
 
 
     private static final GraphicFactory GRAPHIC_FACTORY = AndroidGraphicFactory.INSTANCE;
 
 
-    private static Paint getDefaultCircleFill() {
-        return getPaint(GRAPHIC_FACTORY.createColor(48, 0, 0, 255), 0, Style.FILL);
-    }
 
-    private static Paint getDefaultCircleStroke() {
-        return getPaint(GRAPHIC_FACTORY.createColor(160, 0, 0, 255), 2, Style.STROKE);
-    }
 
-    private static Paint getPaint(int color, int strokeWidth, Style style) {
+    private static Paint getPaint(int color, int strokeWidth, Style style, Context context) {
         Paint paint = GRAPHIC_FACTORY.createPaint();
-        paint.setColor(color);
+        paint.setColor(0x00ffffff & Color.parseColor(MixContext.getInstance().getSettings().getString(context.getString(R.string.pref_item_routecolor_key),context.getString(R.string.color_hint)))|alpha);
         paint.setStrokeWidth(strokeWidth);
         paint.setStyle(style);
         return paint;
@@ -56,13 +55,22 @@ public class MyLocationOverlay extends Layer implements LocationListener {
     public MyLocationOverlay(Context context, MapViewPosition mapViewPosition, DisplayModel displayModel) {
 
 
+
+
         this.mapViewPosition = mapViewPosition;
-        this.circle = new Circle(null, 5, getDefaultCircleFill(), getDefaultCircleStroke());
+        this.circle = new Circle(null, 4, getPaint(GRAPHIC_FACTORY.createColor(48, 0, 0, 255), 0, Style.FILL, context), getPaint(GRAPHIC_FACTORY.createColor(160, 0, 0, 255), 8, Style.STROKE, context));
         this.circle.setLatLong(new LatLong(MixContext.getInstance().getCurLocation().getLatitude(), MixContext.getInstance().getCurLocation().getLongitude()));
 
-        this.circle.setDisplayModel(displayModel);
 
-       // requestRedraw();
+
+        this.innerCircle = new Circle(null, 2f, null, getPaint(GRAPHIC_FACTORY.createColor(160, 0, 0, 255), 2, Style.FILL, context));
+        this.innerCircle.setLatLong(new LatLong(MixContext.getInstance().getCurLocation().getLatitude(), MixContext.getInstance().getCurLocation().getLongitude()));
+
+
+        this.circle.setDisplayModel(displayModel);
+        this.innerCircle.setDisplayModel(displayModel);
+
+
     }
 
     @Override
@@ -73,12 +81,15 @@ public class MyLocationOverlay extends Layer implements LocationListener {
         }
 
         this.circle.draw(boundingBox, zoomLevel, canvas, topLeftPoint);
+        this.innerCircle.draw(boundingBox, zoomLevel, canvas, topLeftPoint);
     }
 
     @Override
     public void onLocationChanged(Location location) {
 
         this.circle.setLatLong(new LatLong(location.getLatitude(),location.getLongitude()));
+        this.innerCircle.setLatLong(new LatLong(location.getLatitude(),location.getLongitude()));
+
 
         requestRedraw();
 
