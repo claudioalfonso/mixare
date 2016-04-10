090f746202b5fc9d8987442a1d4495837674bdb4
Melanie <melanie_werner@gmx.net>
2016-04-10 20:52:19 +0200

    fixed bug in hudView, new RangebarView added

--- src/org/mixare/gui/HudView.java
+++ src/org/mixare/gui/HudView.java
@@ -37,13 +37,6 @@ public class HudView extends RelativeLayout {
     private ImageView positionStatusIcon;
     private ImageView dataSourcesStatusIcon;
     private ImageView sensorsStatusIcon;
-    private SeekBar rangeBar;
-
-    private Radar radar = null;
-    private PaintScreen radarPaintScreen;
-
-    Paint rangeBarLabelPaint = new Paint();
-
 
 
     public HudView(Context context){
@@ -62,41 +55,9 @@ public class HudView extends RelativeLayout {
         init();
     }
 
-    /**
-     * Calculate range base 80.
-     * Mixare support ranges between 0-80km and default value of 5km,
-     * {@link SeekBar SeekBar} on the other hand, is 0-100 base.
-     * This method handles the range conversion between Mixare range and SeekBar progress.
-     * The range resolution on the seek bar is fine in proximity and coarse in the distance.
-     *
-     * @return int Range base 80
-     */
-    public float calcRange(){
-
-        int rangeBarProgress = rangeBar.getProgress();
-        float range = 0;
-
-        if (rangeBarProgress <= 26) {
-            range = rangeBarProgress / 25f;
-        } else if (25 < rangeBarProgress && rangeBarProgress < 50) {
-            range = (1 + (rangeBarProgress - 25)) * 0.38f;
-        } else if (25 == rangeBarProgress) {
-            range = 1;
-        } else if (50 == rangeBarProgress) {
-            range = 10;
-        } else if (50 < rangeBarProgress && rangeBarProgress < 75) {
-            range = (10 + (rangeBarProgress - 50)) * 0.83f;
-        } else {
-            range = (30 + (rangeBarProgress - 75) * 2f);
-        }
-        return range;
-    }
 
     public void init() {
         addView(inflate(getContext(), R.layout.hud_layer, null));
-
-        radar = new Radar();
-        radarPaintScreen = new PaintScreen();
 
         positionStatusText =(TextView) this.findViewById(R.id.positionStatusText);
         dataSourcesStatusText =(TextView) this.findViewById(R.id.dataSourcesStatusText);
@@ -109,14 +70,8 @@ public class HudView extends RelativeLayout {
         sensorsStatusIcon =(ImageView) this.findViewById(R.id.sensorsStatusIcon);
         destinationStatusText = (TextView) this.findViewById(R.id.destinationStatusText);
 
-        initRangeBar();
     }
 
-    private void initRangeBar(){
-        rangeBar = (SeekBar) this.findViewById(R.id.rangeBar);
-        rangeBar.setOnSeekBarChangeListener(onRangeBarChangeListener);
-        setRangeBarProgress(MixContext.getInstance().getSettings().getInt(getContext().getString(R.string.pref_rangeBarProgress), Config.DEFAULT_RANGE_PROGRESS),true);
-    }
 
     public void setDataSourcesStatus(boolean working, boolean problem, String statusText){
         if(statusText!=null && dataSourcesStatusText !=null) {
@@ -151,91 +106,6 @@ public class HudView extends RelativeLayout {
 
     /* ******** Operators - Sensors ****** */
 
-    private SeekBar.OnSeekBarChangeListener onRangeBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-
-        public void onProgressChanged(SeekBar rangeBar, int progress, boolean fromUser) {
-            setRangeBarProgress(progress,false);
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {}
-
-        public void onStopTrackingTouch(SeekBar rangeBar) {
-            setRangeBarProgress(rangeBar.getProgress(),true);
-            //repaint after range changed.
-            MixContext.getInstance().getActualMixViewActivity().repaint();
-            MixContext.getInstance().getActualMixViewActivity().refreshDownload();
-            //Log.d(Config.TAG, "range="+calcRange()+", rangeBarProgress="+rangeBar.getProgress());
-        }
-    };
-
-    public boolean isRangeBarVisible() {
-        return rangeBar != null
-                && rangeBar.getVisibility() == View.VISIBLE;
-    }
-
-    public void hideRangeBar(){
-        if(isRangeBarVisible()){
-            rangeBar.setVisibility(View.INVISIBLE);
-        }
-    }
-    public void showRangeBar(){
-        if(!isRangeBarVisible()){
-            rangeBar.setVisibility(View.VISIBLE);
-        }
-    }
-
-    public void setRangeBarProgress(int rangeBarProgress,boolean finalValue) {
-        rangeBar.setProgress(rangeBarProgress); // set the visual state of the SeekBar
-        float range = calcRange();
-        MixViewDataHolder.getInstance().setRange(range); // save the calculated range in km to be accessed by other processes
-        if(finalValue){
-            MixContext.getInstance().getActualMixViewActivity().getMarkerRenderer().setRadius(range); // set radius of the renderer in KM, TODO remove and access global range from MixViewDataHolder
-            SharedPreferences.Editor editor = MixContext.getInstance().getSettings().edit();
-
-            // store the range of the range bar selected by the user
-            editor.putInt(getContext().getString(R.string.pref_rangeBarProgress), rangeBarProgress);
-            editor.apply(); //or commit()?
-        }
-    }
-
-    public int getRangeBarProgress() {
-        return rangeBar.getProgress();
-    }
-
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        // Draw Radar
-        radarPaintScreen.setCanvas(canvas);
-        radar.paint(radarPaintScreen);
-
-        try {
-            if (isRangeBarVisible()) {
-                rangeBarLabelPaint.setColor(Color.WHITE);
-                rangeBarLabelPaint.setTextSize(14);
-                String startKM, endKM;
-                endKM = "80km";
-                startKM = "0km";
-
-                canvas.drawText(startKM, canvas.getWidth() / 100 * 4,
-                        canvas.getHeight() / 100 * 85, rangeBarLabelPaint);
-                canvas.drawText(endKM, canvas.getWidth() / 100 * 99 + 25,
-                        canvas.getHeight() / 100 * 85, rangeBarLabelPaint);
-
-                int yPos = canvas.getHeight() / 100 * 85;
-                int rangeBarProgress = getRangeBarProgress();
-                if (rangeBarProgress > 92 || rangeBarProgress < 6) { // at beginning/end, jump up because of start/end labels
-                    yPos = canvas.getHeight() / 100 * 80;
-                }
-
-                canvas.drawText(MixViewDataHolder.getInstance().getRangeString(), (canvas.getWidth()) / 100
-                        * rangeBarProgress + 20, yPos, rangeBarLabelPaint);
-            }
-        } catch (Exception ex) {
-            MixContext.getInstance().getActualMixViewActivity().doError(ex, MixViewActivity.GENERAL_ERROR);
-        }
-        super.dispatchDraw(canvas);
-    }
 
     public void setDestinationStatus(Location destination) {
         if(destination!=null && destinationStatusText !=null) {
